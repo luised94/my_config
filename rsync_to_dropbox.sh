c36a949ef8c031c1f606021dcac210aca868e91b
luised94 <luised94@mit.edu>
2024-03-18 15:30:46 -0400

    Created my_config folder for centralized management of dotfiles and nvim

--- rsync_to_dropbox.sh
+++ rsync_to_dropbox.sh
@@ -2,5 +2,5 @@
 # This is found on bashrc file and should be already. Check by echoing variable. 
 # windows_user=$(cmd.exe /C "echo %USERNAME%" 2>/dev/null | tr -d '\r')
 # windows_path="/mnt/c/Users/${windows_user}/Dropbox (MIT)/"
-rsync -av ~/dotfiles/ "${windows_path}dotfiles/"
-find ~/.config/ -type d -name "*nvim*" ! -name "*.bak*" -exec rsync -av {} "${windows_path}neovim/" \;
+rsync -av ~/my_config/ "${windows_path}my_config/"
+#find ~/.config/ -type d -name "*nvim*" ! -name "*.bak*" -exec rsync -av {} "${windows_path}neovim/" \;
