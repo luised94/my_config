c4b9d049f142112ba291d151d2039a8077e90528
luised94 <luised94@mit.edu>
2024-06-07 17:12:06 -0400

    Finished options.lua for now, kept most of the options as kickstart.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,5 +1,6 @@
 -- [[ Setting options ]]
 -- :help vim.opt OR :help option-list
+-- Also see :help '<vim-option>' to see entry for a particular option
 -- loaded using require 'options'
 -- Add number lines and show as relative.
 vim.opt.number = true
@@ -26,3 +27,35 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default, used to show results of LSPs for example
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time, used for crash recovery
+vim.opt.updatetime = 250
+
+-- Modify time neovim waits for keypresses to create commands. Left as default since I am not that fast.
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 1000
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
