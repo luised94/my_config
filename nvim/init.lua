9d6d361e58b02be8cd259ca8e01d3dbee84a42ba
luised94 <luised94@mit.edu>
2024-06-08 22:49:59 -0400

    Added highlight on yank as well as some comments for future plugins, created the dashboard for btw.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,27 @@
-
-
--- print("I used neovim, btw")
+print("I used neovim, btw")
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = false
-require 'lazy-bootstrap'
+
 require 'options'
--- require 'dashboard'
+
+-- require 'keymaps'
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+require 'lazy-bootstrap'
+-- require 'lazy-plugins'
+require 'dashboard'
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
---
+++ nvim/lua/dashboard.lua
@@ -0,0 +1,8 @@
+require("lazy").setup(
+    {
+      "letieu/btw.nvim",
+      config = function()
+        require('btw').setup()
+      end,
+     }
+)
