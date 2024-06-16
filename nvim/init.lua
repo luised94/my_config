f20c4255523c8975d0b353b7821ef1f6146be943
luised94 <luised94@mit.edu>
2024-06-15 21:46:28 -0400

    Added xsel to requirements to install. Added document. Verified that autopairs worked but not all the other plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,5 +18,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 require 'lazy-bootstrap'
+-- Plugins are loaded in the lazy-bootstrap file.
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
