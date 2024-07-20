6380e4c87e7f13c444dfc68983803e5d89a4d711
luised94 <luised94@mit.edu>
2024-07-20 00:17:17 -0400

    Added lsps for bash, r and marksman with modifications to text hover borders. Neovim not completely broken at this point.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,20 +7,13 @@ return {
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
         'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-        -- Useful status updates for LSP.
-        { 'j-hui/fidget.nvim', opts = {} },
-
-        -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-        -- used for completion, annotations and signatures of Neovim apis
-        { 'folke/neodev.nvim', opts = {}, enabled = false },
-        { 'folke/neoconf.nvim', opts = {}, enabled = false },
+        { 'j-hui/fidget.nvim', opts = {} }, -- Useful status updates for LSP.
+        { 'folke/neodev.nvim', opts = {}, enabled = false }, -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
+        { 'folke/neoconf.nvim', opts = {}, enabled = false }, -- used for completion, annotations and signatures of Neovim apis
       },
       config = function()
-        -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-        -- and language tooling communicate in a standardized fashion.
-        -- `:help lsp-vs-treesitter`
-
+        local lspconfig = require 'lspconfig'
+        local lsputil = require 'lspconfig.util'
         --  This function gets run when an LSP attaches to a particular buffer.
         --    That is to say, every time a new file is opened that is associated with
         --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
@@ -88,49 +81,84 @@ return {
             end
           end,
         })
-
+        -- Add borders to floating windows.
+        local border = "rounded" -- other options are single, rounded, shadow, double, solid
+        vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
+        vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
         -- LSP servers and clients are able to communicate to each other what features they support.
         --  By default, Neovim doesn't support everything that is in the LSP specification.
         --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
         --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
+        -- Enable snippet support for lsp. Tell LSP that neovim can handle snippets in completion.
+        capabilities.textDocument.completion.completionItem.snippetSupport = true
+        local lsp_flags = {
+            allow_incremental_sync = true, -- Send modified parts of document to LSP.
+            debounce_text_changes = 150, -- millisecond delay to send text changes
+            }
         -- Enable the following language servers
         --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-
         --  Add any additional override configuration in the following tables. Available keys are:
         --  - cmd (table): Override the default command used to start the server
         --  - filetypes (table): Override the default list of associated filetypes for the server
         --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
         --  - settings (table): Override the default settings passed when initializing the server.
-        --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
         local servers = {
-          -- clangd = {},
-          -- gopls = {},
-          -- pyright = {},
-          -- rust_analyzer = {},
-          -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-
-          -- Some languages (like typescript) have entire language plugins that can be useful:
-          --    https://github.com/pmizio/typescript-tools.nvim
-
-          -- But for many setups, the LSP (`tsserver`) will work just fine
-          -- tsserver = {},
-
-
+          -- Add more servers here. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+          r_language_server = {
+                capabilities = capabilities,
+                flags = lsp_flags,
+                settings = {
+                        r = {
+                          lsp = {
+                              rich_documentation = false,
+                          },
+                        },
+                },
+          },
+          bashls = {
+                capabilities = capabilities,
+                flags = lsp_flags,
+                filetypes = { 'sh', 'bash' },
+          },
+          -- To tell marksman to work with Add to $HOME/.config/marksman the file config.toml with the following text:
+          -- [core]
+          -- markdown.file_extensions = ["md", "markdown", "qmd"]
+          marksman = {
+                    capabilities = capabilities,
+                    filetypes = { 'markdown', 'quarto'},
+                    root_dir = lsputil.root_pattern('.git', '.marksman.toml', '_quarto.yml'),
+          },
+          yamlls = {
+                    capabilities = capabilities,
+                    flags = lsp_flags,
+                    settings = {
+                        yaml = {
+                            schemaStore = {
+                                enable = true,
+                                url ='',
+                            },
+                        },
+                    },
+          },
           lua_ls = {
             -- cmd = {...},
             -- filetypes = { ...},
-            -- capabilities = {},
+            capabilities = capabilities,
+            flags = lsp_flags,
             settings = {
               Lua = {
                 completion = {
                   callSnippet = 'Replace',
                 },
+                runtime = {
+                    version = 'LuaJIT',
+                    plugin = lua_plugin_paths,
+                },
                 -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
                 diagnostics = {
-                    globals = { 'vim' }
+                    globals = { 'vim' , 'quarto', 'pandoc'}
                     --disable = { 'missing-fields' } 
                 },
               },
@@ -151,6 +179,7 @@ return {
         local ensure_installed = vim.tbl_keys(servers or {})
         vim.list_extend(ensure_installed, {
           'stylua', -- Used to format Lua code
+          'shfmt',
         })
         require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
