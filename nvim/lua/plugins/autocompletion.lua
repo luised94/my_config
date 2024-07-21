5dc165d212e015c5cd5dedceb55c005747b073b6
luised94 <luised94@mit.edu>
2024-07-20 22:44:28 -0400

    Modifying autocompletion to integrate with Quarto, R and Zotero.
    Dependencies added:
    telescope-luasnip.nvim, cmp-buffer, cmp-nvim-r, lspkind-nvim,
    cmp-pandoc-references
    
    Other modifications:
    1. Commented out the win32 if statement since I am using neovim via wsl.
    2. Added the dependencies to the sources.
    3. Added formatting to know where the snippets come from.
    4. Added the snippets to load from the snippets directory.
    5. Extended snippets to quarto.
    6. Added cmp sources for cmdline and bash.

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -7,32 +7,35 @@ return {
       -- Snippet Engine & its associated nvim-cmp source
       {
         'L3MON4D3/LuaSnip',
-        build = (function()
+        build = 'make install_jsregexp',
           -- Build Step is needed for regex support in snippets.
           -- This step is not supported in many windows environments.
           -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
+         -- if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+         --   return
+         -- end
         dependencies = {
            {
-             'rafamadriz/friendly-snippets',
-             config = function()
-               require('luasnip.loaders.from_vscode').lazy_load()
-             end,
+            "rafamadriz/friendly-snippets",
+            "benfowler/telescope-luasnip.nvim",
            },
         },
       },
+      'hrsh7th/cmp-buffer',
+      'onsails/lspkind-nvim',
+      'jmbuhr/cmp-pandoc-references', -- For quarto and markdown
+      'jalvesaq/cmp-nvim-r', -- For R
       'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline', -- For command line mode and bash files
     },
     config = function()
+        require('luasnip.loaders.from_vscode').lazy_load()
       -- See `:help cmp`
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
+      local lspkind = require 'lspkind'
       luasnip.config.setup {}
 
       cmp.setup {
@@ -51,7 +54,9 @@ return {
           -- Scroll the documentation window [b]ack / [f]orward
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-y>'] = cmp.mapping.confirm { select = true }, -- Accept ([y]es) the completion. This will expand snippets if the LSP sent a snippet.
+          ['<C-y>'] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true, }, -- Accept ([y]es) the completion. This will expand snippets if the LSP sent a snippet.
           ['<C-Space>'] = cmp.mapping.complete {}, -- Manually trigger a completion from nvim-cmp
 
           -- Moved to the right or left of the expansion in the snippet.
@@ -71,10 +76,59 @@ return {
         },
         sources = {
           { name = 'nvim_lsp' },
+          { name = 'buffer' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'pandoc_references' },
+          { name = 'cmp_nvim_r' },
         },
+        formatting = {
+                    format = lspkind.cmp_format({
+                        mode = 'symbol_text',
+                        maxwidth = 50,
+                        ellipsis_char = '...',
+                        menu = {
+                            nvim_lsp = '[LSP]',
+                            luasnip = '[Snippet]',
+                            buffer = '[Buffer]',
+                            path = '[Path]',
+                            pandoc_references = '[Ref]',
+                            cmp_nvim_r = '[R]',
+                        }
+                    })
+                },
       }
+            -- Command Line completion setup
+      cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' },
+                    { name = 'cmdline' }
+                })
+            })
+      -- Load snippets from snippets directory
+      require('luasnip.loaders.from_vscode').lazy_load()
+      require('luasnip.loaders.from_vscode').lazy_load { paths = { vim.fn.stdpath 'config' .. '/snippets' } }
+
+      -- Extend filetypes
+      luasnip.filetype_extend('quarto', { 'markdown' })
+      luasnip.filetype_extend('rmarkdown', { 'markdown' })
+
+      -- Setup cmp specifically for R
+        require('cmp_nvim_r').setup({
+                filetypes = {'r', 'rmd', 'quarto'},
+                doc_width = 58
+            })
+
+            -- Setup for bash files
+            cmp.setup.filetype({ 'sh', 'bash' }, {
+                sources = cmp.config.sources({
+                        { name = 'nvim_lsp' },
+                        { name = 'luasnip' },
+                        { name = 'buffer' },
+                        { name = 'path' },
+                })
+            })
     end,
   },
 }
