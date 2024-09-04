4c4242d6dafb77227c5296b5a94b8a84a0be7400
luised94 <luised94@mit.edu>
2024-09-03 21:01:13 -0400

    Add comment with rsync command to back up zotero

--- rsync_to_dropbox.sh
+++ rsync_to_dropbox.sh
@@ -4,3 +4,5 @@
 # windows_path="/mnt/c/Users/${windows_user}/Dropbox (MIT)/"
 rsync -av ~/my_config/ "${windows_path}my_config/"
 #find ~/.config/ -type d -name "*nvim*" ! -name "*.bak*" -exec rsync -av {} "${windows_path}neovim/" \;
+# Rsync to dropbox for backup to external hard drive
+#rsync -av --no-perms --no-owner --no-group --no-times --delete "/mnt/c/Users/liusm/Dropbox (MIT)/zotero-storage" "/mnt/d/"
