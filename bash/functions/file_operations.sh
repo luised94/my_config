66fa1130d51f071c045a285af28092336b3bd400
luised94 <luised94@mit.edu>
2024-11-27 11:17:49 -0500

    Modify vim_all function to process files by date, ensuring that most recent modified file is read first.

--- bash/functions/file_operations.sh
+++ bash/functions/file_operations.sh
@@ -52,9 +52,16 @@ vim_all() {
 
     # Collect files
     local files=()
-    while IFS= read -r -d $'\0' file; do
-        files+=("$file")
-    done < <(find "${args["directory"]}" -type f "${exclude_args[@]}" -print0)
+    mapfile -t files < <(
+        find "${args["directory"]}" -type f "${exclude_args[@]}" -printf '%T@ %p\n' | \
+            sort -rn | \
+            cut -d' ' -f2- | \
+            tr -d '\r'
+    )
+
+    #while IFS= read -r -d $'\0' file; do
+    #    files+=("$file")
+    #done < <(find "${args["directory"]}" -type f "${exclude_args[@]}" -printf '%T@ %p\n' | sort -rn | cut -d' ' -f2- | tr '\n' '\0')
     
     if [ ${#files[@]} -eq 0 ]; then
         log_warning "No files found to edit."
