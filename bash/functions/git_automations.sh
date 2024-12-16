1e5ae9b2feb99f97dd7228ecc23a7618adbf17c5
luised94 <luised94@mit.edu>
2024-12-15 22:57:20 -0500

    refactor(git): Reorganize git automation functions
    - Move helper functions to separate files
    - Add formatted_display_helpers.sh and git_automations_helpers.sh
    - Update init.sh to include new helper files
    - Streamline git_automations.sh
    
    This refactor improves code organization and modularity in the
    git automation pipeline.

--- bash/functions/git_automations.sh
+++ bash/functions/git_automations.sh
@@ -2,52 +2,6 @@
 
 [[ -z "$_BASH_UTILS_INITIALIZED" ]] && source "${BASH_SOURCE%/*}/../init.sh"
 
-validate_dir_is_git_repo() {
-    if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-        echo "${OUTPUT_SYMBOLS[ERROR]}This script must be run from a git repository."
-        exit 1
-    fi
-    return 0
-}
-
-has_uncommitted_changes() {
-    validate_dir_is_git_repo
-    # Check if there are any changes staged or unstaged
-    if [[ -n $(git status --porcelain) ]]; then
-        return 0 # Has uncommitted changes
-    else
-        return 1 # No uncommitted changes
-    fi
-}
-
-display_changes() {
-    validate_dir_is_git_repo
-    local branch_name="$1"
-    local staged=false
-    local unstaged=false
-
-    if [[ -n $(git diff --cached --name-status) ]]; then # check for staged
-        staged=true
-    fi
-    if [[ -n $(git diff --name-status) ]]; then # check for unstaged
-        unstaged=true
-    fi
-    
-    if "$staged" || "$unstaged"; then
-        echo "${OUTPUT_SYMBOLS[WARNING]}Local branch '$branch_name' has the following changes:"
-        if "$staged"; then
-            echo "Staged changes:"
-            git diff --cached --stat
-        fi
-        if "$unstaged"; then
-            echo "Unstaged changes:"
-            git diff --stat
-        fi
-    else
-        echo "${OUTPUT_SYMBOLS[INFO]}No changes found on branch '$branch_name'."
-    fi
-}
-
 sync_all_branches() {
     local dry_run=false
     while [[ $# -gt 0 ]]; do
