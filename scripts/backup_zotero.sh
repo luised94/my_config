97c77129a4397934c890c1374adffee90c83ad30
luised94 <luised94@mit.edu>
2025-06-03 23:01:24 -0400

    feat(zotero): Add code to determine destination, add more detailed option - Fix the --delete option!

--- scripts/backup_zotero.sh
+++ scripts/backup_zotero.sh
@@ -10,14 +10,25 @@ if [ ! -z "$DROPBOX_PATH" ]; then
 fi
 
 SOURCE_DIRECTORY="$DROPBOX_PATH/zotero-storage/"
-
+#mapfile -t potential_backup_directories < <(find /mnt/ -type d )
+#for directory in ${potential_backup_directories[@]}; do
+#  echo $directory
+#  if [ -z $directory/backup_drive.txt ]; then
+#    DESTINATION_DIRECTORY="$directory/zotero-storage/"
+#  fi
+#done
 DESTINATION_DIRECTORY="/mnt/f/zotero-storage/"
 
+echo "Debugging: "
+echo "  Source: ${SOURCE_DIRECTORY}"
+echo "  DESTINATION: ${DESTINATION_DIRECTORY}"
 if [ ! -d "$DESTINATION_DIRECTORY" ]; then
  echo "[ERROR] DESTINATION_DIRECTORY does not exist. Set manually or adjust."
  exit 1
 fi
+#mapfile -t number_of_files_destination < <( find $DESTINATION_DIRECTORY -type f | wc -l )
+#mapfile -t number_of_files_source < <( find $DESTINATION_DIRECTORY -type f | wc -l )
 
-rsync -nav -delete "$SOURCE_DIRECTORY" "$DESTINATION_DIRECTORY"
+rsync -nav --progress --stats --delete "$SOURCE_DIRECTORY" "$DESTINATION_DIRECTORY"
 
 # TODO: Add before and after number verification
