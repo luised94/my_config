e6363531e6a44695cd1a407c2e9d87fc51cefb70
Luis <luised94@mit.edu>
2025-07-30 17:29:31 -0400

    fix(tmux): Prevent bashrc from going to home directory, change variable name, adjust window number to zero

--- scripts/start_tmux_session.sh
+++ scripts/start_tmux_session.sh
@@ -2,19 +2,20 @@
 
 REPOSITORY_PATH="$HOME/my_config"
 echo "$REPOSITORY_PATH"
-SESSION_NAME="${REPOSITORY_PATH}_session"
+#SESSION_NAME="${REPOSITORY_PATH}_session"
+SESSION_NAME="session"
 
-tmux new-session -d -s "$SESSION_NAME" -c "$REPO_PATH"
+tmux new-session -d -s "$SESSION_NAME" -c "$REPOSITORY_PATH"
 
 # Name window 1 (editing)
-tmux rename-window -t "$SESSION_NAME:1" 'editing'
+tmux rename-window -t "$SESSION_NAME:0" 'editing'
 
-# Create window 2 (split vertical, both panes in $REPO_PATH)
-tmux new-window -t "$SESSION_NAME:2" -n 'dev' -c "$REPO_PATH"
-tmux split-window -v -t "$SESSION_NAME:2" -c "$REPO_PATH"
+# Create window 2 (split vertical, both panes in $REPOSITORY_PATH)
+tmux new-window -t "$SESSION_NAME:2" -n 'dev' -c "$REPOSITORY_PATH"
+tmux split-window -v -t "$SESSION_NAME:2" -c "$REPOSITORY_PATH"
 
 # Create window 3 (for cluster login, placeholder)
 tmux new-window -t "$SESSION_NAME:3" -n 'cluster'
 
 # Attach to the session
-tmux attach-session -t "$SESSION_NAME"
+#tmux attach-session -t "$SESSION_NAME"
