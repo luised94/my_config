c86e71d732e145e32cf19be478156252c16d8102
Luis <luised94@mit.edu>
2025-08-01 18:01:58 -0400

    feat(git_automations/sync_repos): Create new function for syncing repos in a given directory

--- bash/functions/git_automations.sh
+++ bash/functions/git_automations.sh
@@ -55,6 +55,17 @@ new_worktree() {
 
 }
 
+sync_repos() {
+
+  local repository_location=${2:-$HOME/personal_repos}
+  mapfile -t repository_names < <(find "$repository_location" -mindepth 1 -maxdepth 1 -type d)
+
+  for repository in "${repository_names[@]}"; do
+    echo $repository
+  done
+
+
+}
 ##########################################
 # Must be inspected and simplified greatly.
 ##########################################
