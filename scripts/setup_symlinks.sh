7b1d4d54b723e8ef414be118bb79822ebd5636b1
Luis <luised94@mit.edu>
2025-08-01 15:03:58 -0400

    feat(restructure): Script to setup symlinks

---
+++ scripts/setup_symlinks.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+BASE_DIRECTORY="~/personal_repos/my_config/"
+#FILE_SYMLINK_DIRECTORY="~/"
+DIR_SYMLINK_DIRECTORY="~/.config"
+FILES_TO_SYMLINK=(
+  "dotfiles/bashrc.sh"
+  "dotfiles/vimrc.vim"
+  "nvim"
+)
+
+for filename in "${FILES_TO_SYMLINK[@]}";
+do
+  filepath="$(readlink -f ${BASE_DIRECTORY}${filename})"
+  printf "File to symlink: %s\n" "$filename"
+  printf "Path to file: %s\n" "$filepath"
+
+  if [ -f "$filename" ];
+  then
+      symlink_name=$(readlink -f "$filename" | awk -F/ -v home="$HOME" '{sub(/\..*$/,"",$NF); print home "/." $NF}' )
+      printf "Symbolic link to create: %s\n" "$symlink_name"
+      if [ -h "$symlink_name" ];
+      then
+        printf "Symbolic link exists: %s\n" "$symlink_name"
+        continue
+      fi
+      echo "Creating symlink..."
+      #ln -s "$filepath" "$symlink_name"
+  fi
+  #echo "File or directory do not exist."
+  #exit 1
+done
