84b7955c61429705debc466abc13dfd7f74df6e0
luised94 <luised94@mit.edu>
2025-09-08 23:02:09 -0400

    feat(git_automations): Get repos, see repos already available, add verification and dry run logic - Version 1 with first way that came to mind - Minimal robustness or optimization - Dry-run

--- scripts/git_clone_repos_from_username.sh
+++ scripts/git_clone_repos_from_username.sh
@@ -2,13 +2,17 @@
 # git_download_repos_from_username.sh
 # Download all repos from github using API.
 # Uses username and set root directory
+# Does not access private repos. Requires API key.
+# Date: 2025-09-08
+# Version: 1.0.0
+
 echo "========== Start: ${BASH_SOURCE[0]} =========="
 if ! command -v git >/dev/null 2>&1; then
   echo "Git is not installed" >&2
   return 1
 fi
 
-ROOT_DIRECTORY="~/personal_repos/"
+ROOT_DIRECTORY="$HOME/personal_repos/"
 USERNAME="luised94"
 
 echo "--- Script parameters ---"
@@ -16,4 +20,30 @@ echo "ROOT DIRECTORY: ${ROOT_DIRECTORY}"
 echo "Username: ${USERNAME}"
 echo "-------------------------"
 
-curl -s "https://api.github.com/users/$USERNAME/repos?per_page=100"
+mapfile -t git_repositories < <(
+  curl -s "https://api.github.com/users/$USERNAME/repos?per_page=100" |
+  grep "full_name" | awk -F'[/"]' '{print $5}'
+)
+
+repos_downloaded=$(find "$ROOT_DIRECTORY" -maxdepth 1 -mindepth 1 -type d | grep -v "-" | awk -F'/' '{print $NF}')
+echo "-------------------------"
+echo "Repos downloaded:"
+printf '%s\n' "${repos_downloaded[@]}"
+echo "-------------------------"
+
+for repo in "${git_repositories[@]}"; do
+  echo "Current repo: $repo"
+  if [[ "${repos_downloaded[@]}" =~ "$repo" ]]; then
+    echo "Repo is already downloaded."
+    echo "-------------------------"
+    continue
+  fi
+  echo "Repo is not downloaded."
+  repo_url="https://github.com/$USERNAME/${repo}.git"
+  echo "Repo url to clone: $repo_url"
+  echo "git clone $repo_url $ROOT_DIRECTORY"
+  echo "-------------------------"
+done
+
+#grep -o 'git@[^"]*' | \
+#xargs -L1 git clone
