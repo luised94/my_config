2524e1d3a38dad76a24f86d24f39a873172d2b65
luised94 <luised94@mit.edu>
2026-01-06 20:43:19 -0500

    refactor(deprecated): Remove unused files.

--- bash/scripts/setup_symlinks.sh
+++
@@ -1,5 +0,0 @@
-# Setup symlinks for dotfiles and neovim manually
-# If there are already symlinks setup, you must remove them and backup the files you are currently using. 
-ln -s ~/my_config/dotfiles/bashrc ~/.bashrc
-ln -s ~/my_config/nvim ~/.config/nvim/
-# TODO setup an automation script once I have many more files to setup.
