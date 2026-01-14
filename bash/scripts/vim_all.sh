50208588fcb11a1daef75794f33399f553aa718a
Luis <luised94@mit.edu>
2026-01-14 16:16:07 -0500

    feat(explorations): Move unfinished, deprecated files to the explorations repo. - Clean up.

--- bash/scripts/vim_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[[ -z "$_BASH_UTILS_INITIALIZED" ]] && source "${BASH_SOURCE%/*}/../init.sh"
-
-# Main script wrapper for vim_all function
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    # Script is being run directly
-    vim_all "$@"
-fi
--- scripts/review_holder/bs_test.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-url = "https://www.tutorialspoint.com/index.htm"
-req = requests.get(url)
-soup = BeautifulSoup(req.text, "html.parser")
-print(soup.title)
-
-for link in soup.find_all('a'):
-	print(link.get('href'))
