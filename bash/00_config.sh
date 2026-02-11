6d6986b003db7626aa49253e92443167d80cd03f
luised94 <luised94@mit.edu>
2026-02-10 21:46:00 -0500

    fix(mc/exclude_files): Add *.db to exclude files mc variable.

--- bash/00_config.sh
+++ bash/00_config.sh
@@ -141,7 +141,7 @@ MC_EXCLUDE_DIRS=(
 )
 
 MC_EXCLUDE_FILES=(
-    "*.log" "*.git" "*.tmp" "*.bak" "*.swp" "*.swo" "*.pyc" "*.pyo" "*.o" "*.so"
+    "*.log" "*.db" "*.git" "*.tmp" "*.bak" "*.swp" "*.swo" "*.pyc" "*.pyo" "*.o" "*.so"
     "*.a" "*.class" ".DS_Store" "Thumbs.db" "*repository_aggregate.md"
     "*.gitignore" "*.Rprofile" "*renv.lock"
 )
