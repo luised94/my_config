aaac43ddb1d984948fd738550a543bf7bb47e5ef
luised94 <luised94@mit.edu>
2026-03-20 23:48:31 -0400

    feat(editor): add ftplugin for C buffer-local settings
    - Create ftplugin/c.lua, fires automatically on C filetype
    - Override global tabstop/shiftwidth to 4 for C files only
    - Add colorcolumn at 80 and cindent for C-aware indentation
    - Uses vim.opt_local, no bleed into other filetypes

---
+++ nvim/ftplugin/c.lua
@@ -0,0 +1,10 @@
+-- Fires automatically when a C file is opened.
+-- vim.opt_local scopes everything to the current buffer only.
+
+local opt = vim.opt_local
+
+opt.tabstop     = 4
+opt.shiftwidth  = 4
+opt.expandtab   = true
+opt.colorcolumn = "80"
+opt.cindent     = true
