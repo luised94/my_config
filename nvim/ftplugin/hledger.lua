d7ba52f41b6ef290fad21bb5e9ce7f9d29c0c6e3
luised94 <luised94@mit.edu>
2026-04-01 01:31:50 -0400

    N-1b: add syntax highlighting rules for hledger journals
    * ISO dates at line start
    * Account names (word chars and colons after whitespace)
    * Dollar amounts with sign and punctuation
    * Cleared (*) and pending (!) status markers
    * Comment lines (semicolon at line start)

--- nvim/ftplugin/hledger.lua
+++ nvim/ftplugin/hledger.lua
@@ -7,3 +7,22 @@ vim.opt_local.textwidth = 0
 vim.opt_local.formatoptions:remove("t")
 
 vim.b.ts_highlight = false
+-- === Syntax Highlighting ===
+
+vim.cmd([[
+  syntax match hledgerDate       "^\d\{4\}-\d\{2\}-\d\{2\}"
+  syntax match hledgerAccount    "\s\+[a-zA-Z][a-zA-Z0-9_-]*\(:[a-zA-Z0-9_-]\+\)\+"
+  syntax match hledgerAmount     "\$-\?\d\+\(\,\d\{3\}\)*\(\.\d\+\)\?"
+  syntax match hledgerCleared    "^\d\{4\}-\d\{2\}-\d\{2\}\s\+\zs\*"
+  syntax match hledgerPending    "^\d\{4\}-\d\{2\}-\d\{2\}\s\+\zs!"
+  syntax match hledgerComment    "^;.*$"
+  syntax match hledgerInlineComment "\s;.*$"
+
+  highlight link hledgerDate        Constant
+  highlight link hledgerAccount     Identifier
+  highlight link hledgerAmount      Number
+  highlight link hledgerCleared     Type
+  highlight link hledgerPending     Todo
+  highlight link hledgerComment     Comment
+  highlight link hledgerInlineComment Comment
+]])
